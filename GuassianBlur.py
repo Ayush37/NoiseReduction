03fbfd739a0a166c20faf06dea0d92e2ea83d592
Ayush Gaur <ayush.gaur@ymail.com>
2022-01-26 10:55:21 -0600

    Update GuassianBlur.py

--- GuassianBlur.py
+++ GuassianBlur.py
@@ -13,7 +13,7 @@ def estimate_noise(I):
 
   M = [[1, -2, 1],
        [-2, 4, -2],
-       [1, -2, 1]]
+       [1, -2, 2]]
 
   sigma = np.sum(np.sum(np.absolute(convolve2d(I, M))))
   sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
